4d2a0e07778b1d77283d963a4ba0c69a8a1cddd0
Misaki_Akatsuki <mikuchixip@live.jp>
2016-11-03 18:46:55 +0900

    Update 1.0.1

--- src/Divide Text.ts
+++ src/Divide Text.ts
@@ -16,16 +16,19 @@
     MIT License
 
   バージョン情報
+    2016/11/03 Ver 1.0.1 Update
+      テキストが一文字の時,処理を飛ばすようにした.
+
     2016/11/03 Ver 1.0.0 Release
 */
 
 /// <reference path="C:/Users/RUI/OneDrive/lib/aftereffects.d.ts/ae.d.ts"/>
 
-(function() {
-  const ADBE_TEXT_PROPERTIES:string = "ADBE Text Properties";
-  const ADBE_TEXT_DOCUMENT:string = "ADBE Text Document";
+(function () {
+  const ADBE_TEXT_PROPERTIES: string = "ADBE Text Properties";
+  const ADBE_TEXT_DOCUMENT: string = "ADBE Text Document";
 
-  const isCompActive = function(comp:CompItem) {
+  const isCompActive = function (comp: CompItem) {
     if (!(comp && comp instanceof CompItem)) {
       return false;
     } else {
@@ -33,7 +36,7 @@
     }
   }
 
-  const isLayerSelected = function(layers:Layer[]) {
+  const isLayerSelected = function (layers: Layer[]) {
     if (layers.length === 0) {
       return false;
     } else {
@@ -41,33 +44,36 @@
     }
   }
 
-  const main = function() {
-    const actComp:CompItem = <CompItem>app.project.activeItem;
+  const main = function () {
+    const actComp: CompItem = <CompItem>app.project.activeItem;
     if (!isCompActive(actComp)) {
       return 0;
     }
 
-    const selLayers:Layer[] = <Layer[]>actComp.selectedLayers;
-    let textLayers:TextLayer[] = [];
+    const selLayers: Layer[] = <Layer[]>actComp.selectedLayers;
+    let textLayers: TextLayer[] = [];
 
     // テキストレイヤーを選択している場合はテキストレイヤーのみ抽出,選択していない場合は新規追加する
     if (isLayerSelected(selLayers)) {
-      for(let i=0;i<selLayers.length;i++){
-        if(selLayers[i] instanceof TextLayer){
+      for (let i = 0; i < selLayers.length; i++) {
+        if (selLayers[i] instanceof TextLayer) {
           textLayers.push(<TextLayer>selLayers[i]);
         }
       }
-    }else{
+    } else {
       return 0;
     }
 
-    let curText:string = "";
+    let curText: string = "";
 
-    for(let i=0;i<textLayers.length;i++){
+    for (let i = 0; i < textLayers.length; i++) {
       curText = String(textLayers[i].property(ADBE_TEXT_PROPERTIES).property(ADBE_TEXT_DOCUMENT).value);
+      if(curText.length == 1){
+        continue;
+      }
 
-      for(let j=0;j<curText.length;j++){
-        if(curText[j] == "" || curText[j] == " " || curText[j] == "　"){
+      for (let j = 0; j < curText.length; j++) {
+        if (curText[j] == "" || curText[j] == " " || curText[j] == "　") {
           continue;
         }
         actComp.layers.addText(new TextDocument(curText[j]));
@@ -77,7 +83,7 @@
 
   app.beginUndoGroup("Divide Text");
   main();
-	app.endUndoGroup();
+  app.endUndoGroup();
 
   return 0;
 }).call(this);
